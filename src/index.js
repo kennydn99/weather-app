60b4a1878857aa067422a5e1fa9dc03bbea32271
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-07-18 16:57:35 -0700

    Create function to fetch weather data & process JSON data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import "./styles.css";
 
-const name = "James";
+function fetchWeatherData(location) {
+  const baseURL =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+  const API_KEY = "X9BRD9SNZFM8Z2ZH7TBJRMR56";
+  const url = `${baseURL}${location}?key=${API_KEY}`;
 
-const person = { first: name };
+  const request = new Request(url, { mode: "cors" });
+  return (
+    fetch(request)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("response: ", response);
+        return response.json();
+      })
+      // .then((data) => {
+      //   console.log("data", data);
+      // })
+      .catch((error) => console.log("Fetch error: ", error))
+  );
+}
 
-console.log(person);
+function processData(data) {
+  return {
+    resolvedAddress: data.resolvedAddress,
+    temp: data.currentConditions.temp,
+    conditions: data.currentConditions.conditions,
+    feelslike: data.currentConditions.feelslike,
+    days: data.days.slice(0, 7),
+  };
+}
 
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
-
-sayHelloLinting();
+fetchWeatherData("Arizona").then((data) => {
+  console.log("data: ", data);
+  const myData = processData(data);
+  console.log("myData: ", myData);
+});
