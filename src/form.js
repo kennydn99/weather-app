509bdad4f8da031e22b8aab7c9c7445edd0a27d2
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-07-29 14:05:16 -0700

    Add loading message

--- src/form.js
+++ src/form.js
@@ -3,19 +3,27 @@ import display from "./display";
 
 const form = document.querySelector("#location-form");
 const search = document.querySelector("#searchbar");
+const loadingMessage = document.querySelector(".loading-message");
 
 function setUpForm() {
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
     const location = search.value.trim();
     console.log("User inputed location as: ", location);
-    fetchData.fetchWeatherData(location).then((data) => {
-      console.log("all data: ", data);
+
+    // display loading message
+    loadingMessage.style.display = "block";
+
+    try {
+      const data = await fetchData.fetchWeatherData(location);
       const myData = fetchData.processData(data);
-      console.log("myData: ", myData);
 
       display.renderData(myData);
-    });
+    } catch (error) {
+      console.error("There was an error fetching weather data:", error);
+    } finally {
+      loadingMessage.style.display = "none";
+    }
   });
 }
 
